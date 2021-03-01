5e60083e904f9b35ada4129d0523d965e565cba6
davide schiaroli <davide.schiaroli2@studio.unibo.it>
2021-03-01 14:04:26 +0100

    Now atm class does not extends noAtm class

--- pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -34,4 +34,36 @@ public class SimpleBankAccountWithAtmTest{
 		bankAccount.withdrawWithAtm(accountHolder.getId(), 100);
 		assertEquals(100, bankAccount.getBalance());
 	}
+
+	@Test
+	void testInitialBalance() {
+		assertEquals(0, bankAccount.getBalance());
+	}
+
+	@Test
+	void testDeposit() {
+		bankAccount.deposit(accountHolder.getId(), 100);
+		assertEquals(100, bankAccount.getBalance());
+	}
+
+	@Test
+	void testWrongDeposit() {
+		bankAccount.deposit(accountHolder.getId(), 100); //Questo va
+		bankAccount.deposit(2, 50); //Questo non va
+		assertEquals(100, bankAccount.getBalance()); //
+	}
+
+	@Test
+	void testWithdraw() {
+		bankAccount.deposit(accountHolder.getId(), 100);
+		bankAccount.withdraw(accountHolder.getId(), 70);
+		assertEquals(30, bankAccount.getBalance());
+	}
+
+	@Test
+	void testWrongWithdraw() {
+		bankAccount.deposit(accountHolder.getId(), 100);
+		bankAccount.withdraw(2, 70);
+		assertEquals(100, bankAccount.getBalance());
+	}
 }
